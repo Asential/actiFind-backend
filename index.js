d028937bf546b06d89d8de05c19661cb2ef190c2
Asential <dhruvsingh2529@gmail.com>
2023-04-24 19:03:46 +0530

    Added MongoAtlas Connection, Post Schema & Routes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import userRouter from "./routes/user.js";
-
+import postRouter from "./routes/posts.js";
 
 const app = express();
 
@@ -12,10 +13,14 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 // Routes
+app.use("/posts", postRouter);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb://0.0.0.0:27017/actiFindDatabase';
+
+// ---!!! Unsecure Connection !!!--- Enviroment Variables to be added. 
+const CONNECTION_URL = 'mongodb+srv://asential:asential123@cluster0.dvh0whd.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
+mongoose.set("strictQuery", false);
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
