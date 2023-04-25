92b52705edb4906df3f865aad772fb201fa27c6b
Asential <dhruvsingh2529@gmail.com>
2023-04-25 10:30:05 +0530

    Added Enviroment Variables

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import userRouter from "./routes/user.js";
 import postRouter from "./routes/posts.js";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-// ...
+// Middleware
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -16,12 +18,9 @@ app.use(cors());
 app.use("/posts", postRouter);
 app.use("/user", userRouter);
 
-
-// ---!!! Unsecure Connection !!!--- Enviroment Variables to be added. 
-const CONNECTION_URL = 'mongodb+srv://asential:asential123@cluster0.dvh0whd.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 mongoose.set("strictQuery", false);
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
